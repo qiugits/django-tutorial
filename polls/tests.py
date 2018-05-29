1b628ed401ceb09398e21cc5a17acdf2a76415ab
qiugits <qiu.gits@gmail.com>
2018-05-30 02:36:14 +0900

    Add tests

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,91 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+from django.utils import timezone
+
+from .models import Question
+
+
+class QuestionModelTests(TestCase):
+
+    def test_was_published_recently_with_different_pub_dates(self):
+        """
+        was_published_recently() returns False for questions whose pub_date is in the future
+        was_published_recently() returns False for questhions whose pub_date is older than 1 day.
+        was_published_recently() returns True for questhions whose pub_date is within the last day.
+        """
+        now = timezone.now()
+        time = now + datetime.timedelta(days=30)
+        question = Question(pub_date=time)
+        self.assertIs(question.was_published_recently(), False)
+
+        time = now - datetime.timedelta(days=1, seconds=1)
+        question = Question(pub_date=time)
+        self.assertIs(question.was_published_recently(), False)
+
+        time = now - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        question = Question(pub_date=time)
+        self.assertIs(question.was_published_recently(), True)
+
+
+def create_question(question_text, days):
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
+
+
+class QuestionIndexViewTests(TestCase):
+    def test_no_questions(self):
+        """
+        If no questions exists.
+        """
+        response = self.client.get(reverse('polls:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+    def test_past_question(self):
+        create_question(question_text="Past question", days=-30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            ['<Question: Past question>']
+        )
+
+    def test_future_question(self):
+        create_question(question_text="Future question", days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+    def test_future_question_and_past_question(self):
+        create_question(question_text="Past question", days=-30)
+        create_question(question_text="Future question", days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            ['<Question: Past question>']
+        )
+
+    def test_two_past_questions(self):
+        create_question(question_text="Past question 1", days=-5)
+        create_question(question_text="Past question 2", days=-30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            ['<Question: Past question 1>', '<Question: Past question 2>']
+        )
+
+
+class QuestionDetailViewTests(TestCase):
+    def test_future_question(self):
+        future_question = create_question('Future question', days=5)
+        url = reverse('polls:detail', args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        past_question = create_question('Past question', days=-5)
+        url = reverse('polls:detail', args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.utils import timezone
 from django.views import generic
 
 from .models import Choice, Question
@@ -12,12 +13,18 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """ Return the latest 5 published questions. """
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """ Excludes any questions that aren't published yet. """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
